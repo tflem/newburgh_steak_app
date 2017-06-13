4483791e358ce926a8e1ef0d700d3ecb936af419
Tim Fleming <tfleming26@hotmail.com>
2017-06-13 11:08:09 -0600

    Heroku troubleshooting

--- newburgh_steak/newburgh_steak/wsgi.py
+++ newburgh_steak/newburgh_steak/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newburgh_steak.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
